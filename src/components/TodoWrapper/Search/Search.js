46b1aec6d29abdfc986541c520488af2c538e88e
James Mac William <jimmy.macwilliam@gmail.com>
2018-07-15 23:45:02 -0400

    Add handler for input submit
    - change Search api to pass value back to prop functions rather than
    event

--- src/components/TodoWrapper/Search/Search.js
+++ src/components/TodoWrapper/Search/Search.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Search extends Component {
+  constructor() {
+    super()
+    this.input = React.createRef()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.submit(this.input.current.value)
+    this.input.current.value = ''
+  }
+  handleInput(e) {
+    this.props.update(this.input.current.value)
+  }
   render() {
     return (
       <div className="searchWrapper">
-        <input className="searchInput" type="text" placeholder="Enter a new ToDo item..."></input>
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            ref={ this.input }
+            className="searchInput"
+            type="text"
+            placeholder="Enter a new ToDo item..."
+            onInput={ this.handleInput }
+          />
+        </form>
       </div>
     )
   }
+}
+
+Search.propTypes = {
+  update: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired
 }
 
 export default Search
--- src/components/TodoWrapper/TodoWrapper.js
+++ src/components/TodoWrapper/TodoWrapper.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import Search from './Search'
 
 class TodoWrapper extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: '',
+      todos: []
+    }
+    this.updateSearch = this.updateSearch.bind(this)
+    this.addTodo = this.addTodo.bind(this)
+  }
+  updateSearch(val) {
+    this.setState({ query: val })
+  }
+  addTodo(val) {
+    this.setState({ todos: this.state.todos.push(val) })
+  }
   render() {
     return (
       <div className="todoWrapper">
-        <Search />
+        <Search
+          update={ this.updateSearch }
+          submit={ this.addTodo }
+        />
       </div>
     )
   }
