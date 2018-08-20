0d2158de00902782a3d6b099f89d0cc007d6866f
James Mac William <jimmy.macwilliam@gmail.com>
2018-08-19 22:28:50 -0500

    add 2 auto scaling groups + security groups (1 in each az)

---
+++ deploy/app-servers.tf
@@ -0,0 +1,51 @@
+resource "aws_autoscaling_group" "asg_app1" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # interpolate the LC into the ASG name so it always forces an update
+  name                      = "asg-app - ${aws_launch_configuration.lc_app.name}"
+  max_size                  = 2
+  min_size                  = 1
+  wait_for_elb_capacity     = 1
+  desired_capacity          = 1
+  health_check_grace_period = 150
+  health_check_type         = "ELB"
+  launch_configuration      = "${aws_launch_configuration.lc_app.id}"
+  load_balancers            = ["${aws_elb.elb_app.id}"]
+
+  vpc_zone_identifier = ["${data.aws_availability_zones.available[0].id}"]
+}
+
+resource "aws_autoscaling_group" "asg_app2" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # interpolate the LC into the ASG name so it always forces an update
+  name                      = "asg-app - ${aws_launch_configuration.lc_app.name}"
+  max_size                  = 2
+  min_size                  = 1
+  wait_for_elb_capacity     = 1
+  desired_capacity          = 1
+  health_check_grace_period = 150
+  health_check_type         = "ELB"
+  launch_configuration      = "${aws_launch_configuration.lc_app.id}"
+  load_balancers            = ["${aws_elb.elb_app.id}"]
+
+  vpc_zone_identifier = ["${data.aws_availability_zones.available[1].id}"]
+}
+
+resource "aws_launch_configuration" "lc_app" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  image_id      = "${var.ami}"
+  instance_type = "t2.micro"
+
+  # Our Security group to allow HTTP and SSH access
+  security_groups = ["${aws_security_group.nginx-sg.id}"]
+
+  key_name  = "deployer"
+}
--- deploy/security-groups.tf
+++ deploy/security-groups.tf
@@ -18,3 +18,32 @@ resource "aws_security_group" "elb-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx_sg"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.network_address_space}"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
