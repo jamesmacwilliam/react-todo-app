7380f94f59bb37876cf9550f9f247cd700b1cfa6
James Mac William <jimmy.macwilliam@gmail.com>
2018-08-24 12:14:08 -0500

    getting started with docker

---
+++ container.sh
@@ -0,0 +1,2 @@
+docker build -t="test" -f docker/Dockerfile .
+docker run -t -i test /bin/bash
---
+++ puppet/docker_install_agent.sh
@@ -0,0 +1,2 @@
+rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
+yum install -y puppet-agent
